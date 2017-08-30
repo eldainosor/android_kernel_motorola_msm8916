c79347c43bc8e413bfe1dc395425af3363fcc020
Carlos Arriaga <carlosarriagacm@gmail.com>
2017-08-29 19:31:17 -0500

    icmp.c: fix upstream

--- net/ipv6/icmp.c
+++ net/ipv6/icmp.c
@@ -90,11 +90,7 @@ static void icmpv6_err(struct sk_buff *skb, struct inet6_skb_parm *opt,
 	struct net *net = dev_net(skb->dev);
 
 	if (type == ICMPV6_PKT_TOOBIG)
-<<<<<<< HEAD
-		ip6_update_pmtu(skb, net, info, 0, 0, INVALID_UID);
-=======
-		ip6_update_pmtu(skb, net, info, 0, 0, sock_net_uid(net, NULL));
->>>>>>> b5760d1... net: inet: Support UID-based routing in IP protocols.
+                ip6_update_pmtu(skb, net, info, 0, 0, sock_net_uid(net, NULL));
 	else if (type == NDISC_REDIRECT)
 		ip6_redirect(skb, net, 0, 0, sock_net_uid(net, NULL));
 
